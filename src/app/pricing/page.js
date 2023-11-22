2a8299e680292fc51014c1c984e2d6483127aadc
rajesh <rajesh@kpost.com>
2023-11-22 12:36:21 +0530

    404 Inner Page - Design completed

--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -1,31 +1,37 @@
-// features.js
-import React from "react";
-import Link from 'next/link';
-import Header from "../header";
-import Footer from "../footer";
-const Pricing = () => {
-  return (
-    <div>
-    <Header />
-    <div className="bg-gray-300 py-24 sm:py-20 ">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:text-center">
-            {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
-            <h1 className="mt-2  text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl">
-            Pricing Page            
-            </h1>
-            <h3 className="mt-5  text-2xl font-bold tracking-tight text-gray-500 sm:text-2xl">
-              Coming Soon             
-            </h3>
-            <p className="mt-2 text-gray-500">Maintenance page</p>
-            
+import Header from "../header"
+import Footer from "../footer"
+export default function Pricing() {
+    return (
+      <>
+        {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full">
+          <body class="h-full">
+          ```
+        */}
+        <Header /> 
+        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <p className="text-base font-semibold text-indigo-600">404</p>
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+            <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="/"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Go back home
+              </a>
+              <a href="#" className="text-sm font-semibold text-gray-900">
+                Contact support <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
           </div>
-         
-        </div>
-      </div>
-    <Footer />
-  </div>
-  );
-};
-
-export default Pricing;
+        </main>
+        <Footer />
+      </>
+    )
+  }
+  
